9ce787d178c4d3b77c4686081b8a9a5276adc1be
Nan Liu <nan@puppetlabs.com>
2012-01-19 15:53:13 -0800

    Change parameters.

--- manifests/data.pp
+++ manifests/data.pp
@@ -1,30 +1,30 @@
 class memcached::data {
-  $package = {}
-  $config  = {}
-  $service = {}
-  $setting = {}
+  $memcached_package = {}
+  $memcached_config  = {}
+  $memcached_service = {}
+  $memcached_setting = {}
 
-  $package['ensure']          = present
-  $setting['logfile']         = '/var/log/memcached.log'
-  $setting['max_memory']      = false
-  $setting['listen_ip']       = '0.0.0.0'
-  $setting['tcp_port']        = '11211'
-  $setting['udp_port']        = '11211'
-  $setting['max_connections'] = '8192'
+  $memcached_package['ensure']          = present
+  $memcached_setting['logfile']         = '/var/log/memcached.log'
+  $memcached_setting['max_memory']      = false
+  $memcached_setting['listen_ip']       = '0.0.0.0'
+  $memcached_setting['tcp_port']        = '11211'
+  $memcached_setting['udp_port']        = '11211'
+  $memcached_setting['max_connections'] = '8192'
 
   # Use osfamily if possible:
   if ($::osfamily == 'Debian') or ($::operatingsystem =~ /(?i:ubuntu|debian)/) {
-    $package['name']    = 'memcached'
-    $service['name']    = 'memcached'
-    $config['path']     = '/etc/memcached.conf'
-    $config['template'] = "${module_name}/memcached.conf.erb"
-    $user               = 'nobody'
+    $memcached_package['name']    = 'memcached'
+    $memcached_service['name']    = 'memcached'
+    $memcached_config['path']     = '/etc/memcached.conf'
+    $memcached_config['template'] = "${module_name}/memcached.conf.erb"
+    $memcached_user               = 'nobody'
   } elsif ($::osfamily == 'Redhat') or ($::operatingsystem =~ /(?i:redhat|centos)/) {
-    $package['name']    = 'memcached'
-    $service['name']    = 'memcached'
-    $config['path']     = '/etc/sysconfig/memcached'
-    $config['template'] = "${module_name}/memcached_sysconfig.erb"
-    $user               = 'memcached'
+    $memcached_package['name']    = 'memcached'
+    $memcached_service['name']    = 'memcached'
+    $memcached_config['path']     = '/etc/sysconfig/memcached'
+    $memcached_config['template'] = "${module_name}/memcached_sysconfig.erb"
+    $memcached_user               = 'memcached'
   } else {
     fail("Unsupported platform: ${::operatingsystem}")
   }
