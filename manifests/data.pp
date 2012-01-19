8bf836ef4ec142db6d2970332f4762d367c5bdd6
Nan Liu <nan@puppetlabs.com>
2012-01-19 15:50:21 -0800

    Fix trailing comma.

--- manifests/data.pp
+++ manifests/data.pp
@@ -1,10 +1,10 @@
 class memcached::data {
-  $package['ensure']          = present,
-  $setting['logfile']         = '/var/log/memcached.log',
-  $setting['max_memory']      = false,
-  $setting['listen_ip']       = '0.0.0.0',
-  $setting['tcp_port']        = '11211',
-  $setting['udp_port']        = '11211',
+  $package['ensure']          = present
+  $setting['logfile']         = '/var/log/memcached.log'
+  $setting['max_memory']      = false
+  $setting['listen_ip']       = '0.0.0.0'
+  $setting['tcp_port']        = '11211'
+  $setting['udp_port']        = '11211'
   $setting['max_connections'] = '8192'
 
   # Use osfamily if possible:
